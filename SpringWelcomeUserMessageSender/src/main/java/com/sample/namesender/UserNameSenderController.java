3ec265af1f9a9577a4d75d64f4d6c9d2b46f44d3
Amlan Saha <amlan18hit@gmail.com>
2017-11-04 11:34:21 +0530

    Enhancement and Exception handling.
    Some enhancement done for exception handling while making the REST call.

--- SpringWelcomeUserMessageSender/src/main/java/com/sample/namesender/UserNameSenderController.java
+++ SpringWelcomeUserMessageSender/src/main/java/com/sample/namesender/UserNameSenderController.java
@@ -1,5 +1,6 @@
 package com.sample.namesender;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +8,10 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.http.HttpMethod;
 import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.client.HttpStatusCodeException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 
 
 @RestController
@@ -15,11 +20,33 @@ public class UserNameSenderController {
 	@Autowired
 	RestTemplate restTemplate;
 	
+	@Value("${baseurl.namereceiver}")
+	String baseUrl;
+	
+	@Value("${urlpattern.getWelcomeMessageUrlPattern}")
+	String getWelcomeMessagePattern;
+	
+	ResponseEntity<String> response = null;
+	
 	@RequestMapping("/getWelcomeMessage")
-	public String getWelcomeMessageForUser(@RequestParam("name")String name){
+	public ResponseEntity<String> getWelcomeMessageForUser(@RequestParam("name")String name){
 		System.out.println("Getting welcome message for user name " + name);
-		String response = restTemplate.exchange("http://localHost:8091/getWelcomeMessageForUser/{name}", HttpMethod.GET, null, String.class, name).getBody();
-		return response;
+		try{
+			 response = restTemplate.exchange(baseUrl + getWelcomeMessagePattern, HttpMethod.GET, null, String.class, name);
+		}
+		catch (HttpStatusCodeException ex) {
+		    int statusCode = ex.getStatusCode().value();
+		    
+		    System.out.println(statusCode);
+		    if(statusCode >= 500)
+		    	return new ResponseEntity<String>("Something Wrong Happened", HttpStatus.INTERNAL_SERVER_ERROR);
+		    else if(statusCode >= 404)
+		    	return new ResponseEntity<String>("Welcome Message URL pattern For Receiver NOT configured properly", HttpStatus.NOT_FOUND);
+		    
+		}
+	
+		
+		return new ResponseEntity<String>(response.getBody(), HttpStatus.OK);
 	}
 	
 	
